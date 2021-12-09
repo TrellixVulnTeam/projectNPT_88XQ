829c6e50876b241e836b1f15b07b80459220ca1b
tungkthd01 <43209840+tungkthd01@users.noreply.github.com>
2021-12-09 14:01:27 +0700

    pagenations

--- backend/Blog-master/blog/commons/paginations.py
+++ backend/Blog-master/blog/commons/paginations.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 
+from rest_framework import pagination
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 class PaginationAPIView(APIView):
     queryset = None
@@ -32,3 +35,22 @@ class PaginationAPIView(APIView):
         """
         assert self.paginator is not None
         return self.paginator.get_paginated_response(data)
+
+class CustomPagination(pagination.PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'perpage'
+    def get_paginated_response(self, data):
+        response = Response(data)
+        response['count'] = self.page.paginator.count
+        response['current'] = self.page.number
+        response['next'] = self.get_next_link()
+        response['previous'] = self.get_previous_link()
+        pagination = {
+        'page': self.page.number,
+        'total_page': self.page.paginator.num_pages,
+        'total_row': self.page.paginator.count,
+        'perpage': self.page.paginator.per_page
+
+        }
+        content = {'pagination': pagination, 'data': data}
+        return Response(content)
